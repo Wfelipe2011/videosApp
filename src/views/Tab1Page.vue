9ddc94e4e984c2e600d4e60bb212043e1cc01066
Wilson <wfelipe2011@gmail.com>
2022-06-05 20:02:29 -0300

    criado sobre

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -2,22 +2,10 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Perfil</ion-title>
+        <ion-title>Tab 1</ion-title>
       </ion-toolbar>
     </ion-header>
-    <ion-content :fullscreen="true">
-      <div class="card">
-        <img
-          class="profile"
-          src="https://avatars.githubusercontent.com/u/60045911?v=4"
-          alt="Wilson"
-        />
-        <div class="card-content">
-          <h1>Wilson Felipe</h1>
-          <p>Desenvolvedor FullStack na Tegra</p>
-        </div>
-      </div>
-    </ion-content>
+    <ion-content :fullscreen="true"> </ion-content>
   </ion-page>
 </template>
 
@@ -36,25 +24,3 @@ export default defineComponent({
   components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage },
 });
 </script>
-
-<style>
-.profile {
-  width: 100px;
-  height: 100px;
-  border-radius: 50%;
-  margin: 0 auto;
-}
-
-.card {
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  padding: 15px;
-  margin: 10px;
-}
-
-.card-content {
-  padding: 15px;
-}
-</style>
--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -4,18 +4,18 @@
       <ion-router-outlet></ion-router-outlet>
       <ion-tab-bar slot="bottom">
         <ion-tab-button tab="TabProfile" href="/tabs/TabProfile">
-          <ion-icon :icon="personCircleOutline" />
+          <ion-icon :icon="ellipse" />
           <ion-label>Perfil</ion-label>
         </ion-tab-button>
 
         <ion-tab-button tab="tab2" href="/tabs/tab2">
           <ion-icon :icon="triangle" />
-          <ion-label>Tab 2</ion-label>
+          <ion-label>Wilson</ion-label>
         </ion-tab-button>
 
         <ion-tab-button tab="tab3" href="/tabs/tab3">
-          <ion-icon :icon="square" />
-          <ion-label>Tab 3</ion-label>
+          <ion-icon :icon="personCircleOutline" />
+          <ion-label>Sobre</ion-label>
         </ion-tab-button>
       </ion-tab-bar>
     </ion-tabs>
